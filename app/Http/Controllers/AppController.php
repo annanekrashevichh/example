3266e10a7ba1d8d45bd1488601942eaf0bf18e46
Anna <annanekrashevichh@gmail.com>
2023-04-11 20:15:37 +0300

    Change and push file

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -10,4 +10,6 @@ use Illuminate\Routing\Controller as BaseController;
 class AppController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private $variable;
 }
